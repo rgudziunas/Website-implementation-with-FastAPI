bdc59f5550678c19e97cfcca4802cb4b854ada95
rgudziunas <rokas123gudziunas@gmail.com>
2025-11-13 22:59:28 +0200

    thrthrth

--- database.py
+++ database.py
@@ -1,19 +1,11 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-# Get the MySQL URL from environment
-URL_DATABASE = os.getenv("MYSQL_URL")
+# Temporarily hardcode the connection string to test
+URL_DATABASE = "mysql+pymysql://root:kLFmcKOQaKdogRKnPvWfQENxkarAhzip@crossover.proxy.rlwy.net:34987/railway"
 
-if URL_DATABASE:
-    # Convert mysql:// to mysql+pymysql://
-    URL_DATABASE = URL_DATABASE.replace("mysql://", "mysql+pymysql://")
-    print("Connected to Railway MySQL")
-else:
-    # Fallback - this will fail in production
-    URL_DATABASE = 'mysql+pymysql://root:rokas@localhost:3306/saitynai'
-    print("ERROR: MYSQL_URL not found in environment")
+print("Using hardcoded Railway MySQL connection")
 
 engine = create_engine(URL_DATABASE, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
