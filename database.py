686990fc4e8f5ab8da88c1f4987f28d1d075de8a
rgudziunas <rokas123gudziunas@gmail.com>
2025-11-13 23:32:11 +0200

    jggh

--- database.py
+++ database.py
@@ -6,9 +6,7 @@ from sqlalchemy.ext.declarative import declarative_base
 # Get from environment variable (for production)
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-if not DATABASE_URL:
-    # For local development (you can update this with your actual Supabase URL for testing)
-    DATABASE_URL = 'postgresql://postgres:Rokas958@db.sgiebgscreirsoddijdc.supabase.co:5432/postgres'
+
 
 # Fix for Supabase (they give postgres:// but SQLAlchemy needs postgresql://)
 if DATABASE_URL.startswith("postgres://"):
