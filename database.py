538005b70d3cb7f96822e476cb16900b65e22c65
rgudziunas <rokas123gudziunas@gmail.com>
2025-11-13 22:20:21 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,11 +1,16 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_DATABASE = 'mysql+pymysql://root:rokas@localhost:3306/saitynai'
+# Use Railway's MYSQL_URL if available, otherwise use localhost for development
+URL_DATABASE = os.getenv(
+    "MYSQL_URL",  # Railway provides this
+    'mysql+pymysql://root:rokas@localhost:3306/saitynai'  # Local fallback
+)
 
 engine = create_engine(URL_DATABASE, pool_pre_ping=True)
 
-SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
